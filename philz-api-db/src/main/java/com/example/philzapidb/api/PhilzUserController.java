3d6e5a09c26781a11462a9cbcc06d4ffa90557ae
Ryan Ereno <ryanaereno@gmail.com>
2021-11-30 18:07:09 -0800

    updated PhilzUserController

--- philz-api-db/src/main/java/com/example/philzapidb/api/PhilzUserController.java
+++ philz-api-db/src/main/java/com/example/philzapidb/api/PhilzUserController.java
@@ -2,6 +2,7 @@ package com.example.philzapidb.api;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,57 @@ import lombok.Setter;
 @RestController
 public class PhilzUserController {
 
+    private final UserRepository repository;
+
+    PhilzUserController(UserRepository repository){
+        this.repository = repository;
+    }
+
+    // Aggregate root
+  // tag::get-aggregate-root[]
+  @GetMapping("/users")
+  List<User> all() {
+    return repository.findAll();
+  }
+  // end::get-aggregate-root[]
+
+  @PostMapping("/users")
+   User newUser(@RequestBody User newUser) {
+    return repository.save(newUser);
+  }
+
+  // Single item
+  
+  @GetMapping("/users/{id}")
+  User one(@PathVariable Long id) {
+    return repository.findById(id)
+    .orElseThrow(() -> {
+        System.out.println("Could not find User " + id);
+        return null;
+    });
+  }
+
+  @PutMapping("/users/{id}")
+  User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
     
+    return repository.findById(id)
+      .map(user -> {
+        user.setName(newUser.getName());
+        user.setEmail(newUser.getEmail());
+        user.setMobile(newUser.getMobile());
+        user.setPassword(newUser.getPassword());
+        return repository.save(user);
+      })
+      .orElseGet(() -> {
+        newUser.setId(id);
+        return repository.save(newUser);
+      });
+  }
+
+  @DeleteMapping("/users/{id}")
+  void deleteUser(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
 }
+
+    
