2a4340bfe330ac930df2c317f8d83b8a158c4ffa
Ngan Luu <ngan.luu@sjsu.edu>
2021-12-03 20:12:15 -0800

    Login Register api + front end

---
+++ philz-customer-v2/src/test/java/com/example/philzcustomer/PhilzCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.philzcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilzCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
