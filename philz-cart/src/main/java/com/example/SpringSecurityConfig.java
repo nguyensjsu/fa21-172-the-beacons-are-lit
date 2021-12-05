1a8de008ef6f2f1c7bbb5cae330b8daae8a02254
Mary <mary.markart@sjsu.edu>
2021-12-05 01:05:55 -0800

    Edits to SpringSecurity

--- philz-cart/src/main/java/com/example/SpringSecurityConfig.java
+++ philz-cart/src/main/java/com/example/SpringSecurityConfig.java
@@ -14,22 +14,36 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.cors.*;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-    	http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+		http.csrf().disable();
+		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
 	}
-	
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE"));
+		// The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+		configuration.setAllowCredentials(true);
+		// setAllowedHeaders is important! Without it, OPTIONS preflight request
+		// will fail with 403 Invalid CORS request
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 	// @Override
 	// protected void configure(HttpSecurity http) throws Exception {
 	// 	http.cors();
 	// 	http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 	// 			.authorizeRequests().antMatchers("/*").permitAll().anyRequest().authenticated();
-		
-	// }
 
 }
