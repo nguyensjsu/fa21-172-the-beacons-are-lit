205640365e56c2e80be2021eb55841526730d295
hughvo39@gmail.com <hieu.vo@sjsu.edu>
2021-12-05 21:49:42 -0800

    RabbitMQ inital stuff

--- philz-cart/src/main/java/com/example/PhilzProductApplication.java
+++ philz-cart/src/main/java/com/example/PhilzProductApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.rabbitmq.Receiver;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
@@ -12,16 +13,58 @@ import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFacto
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class PhilzProductApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhilzProductApplication.class, args);
-		//SpringApplication.run(PhilzPaymentApplication.class, args);
+
+	static final String topicExchangeName = "spring-boot-exchange";
+
+	static final String queueName = "spring-boot";
+  
+	@Bean
+	Queue queue() {
+	  return new Queue(queueName, false);
+	}
+  
+	@Bean
+	TopicExchange exchange() {
+	  return new TopicExchange(topicExchangeName);
+	}
+  
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+	  return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	}
+  
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+		MessageListenerAdapter listenerAdapter) {
+	  SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+	  container.setConnectionFactory(connectionFactory);
+	  container.setQueueNames(queueName);
+	  container.setMessageListener(listenerAdapter);
+	  return container;
+	}
+  
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+	  return new MessageListenerAdapter(receiver, "receiveMessage");
 	}
 
 	@Bean
@@ -29,6 +72,12 @@ public class PhilzProductApplication {
 		Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
 		factory.setResources(new Resource[]{new ClassPathResource("json/drinks.json")});
 		return factory;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilzProductApplication.class, args);
+		//SpringApplication.run(PhilzPaymentApplication.class, args);
+		//SpringApplication.run(MessagingRabbitmqApplication.class, args).close();
 	}
 
 }
