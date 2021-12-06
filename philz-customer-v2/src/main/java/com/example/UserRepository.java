51c8d50a688b1354469391367c2019c435f6e036
hughvo39@gmail.com <hieu.vo@sjsu.edu>
2021-12-06 13:30:53 -0800

    Application property update

--- philz-customer-v2/src/main/java/com/example/UserRepository.java
+++ philz-customer-v2/src/main/java/com/example/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example;
 
-import org.springframework.data.jpa.repository.CrudRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
--- philz-customer-v2/src/main/java/com/example/UserServiceImpl.java
+++ philz-customer-v2/src/main/java/com/example/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.codehaus.jettison.json.JSONException;
